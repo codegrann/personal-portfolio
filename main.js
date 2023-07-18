053f40fefd200c3aa07a7a7f2fa8c7c3030b2294
codegrann <omondihills02@gmail.com>
2023-07-18 22:58:30 +0300

    .

--- main.js
+++ main.js
@@ -1,2 +1,5 @@
 // document.querySelector('body').style.backgroundColor="red";
-window.open('https://app.surveoo.com/en/surveys', 'survey')
+// window.open('https://app.surveoo.com/en/surveys', 'survey')
+// document.write(window.innerHeight)
+// document.write(document.documentElement.clientHeight)
+document.write(location);
