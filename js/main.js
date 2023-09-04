428bc4603c5e0fbdc9ec451a3d7e7422b672cadc
codegrann <omondihills02@gmail.com>
2023-09-04 16:21:34 +0300

    successfully collapsed the nav menu when an item is clicked

--- js/main.js
+++ js/main.js
@@ -1,1 +1,17 @@
+document.addEventListener("DOMContentLoaded", function () {
+  // Get all navigation links
+  const navLinks = document.querySelectorAll(".nav-link");
 
+  // Attach a click event listener to each navigation link
+  navLinks.forEach(function (link) {
+    link.addEventListener("click", function () {
+      // Check if the navigation menu is expanded
+      const navbarCollapse = document.getElementById("navbar-collapse-toggle");
+      if (window.getComputedStyle(navbarCollapse).display !== "none") {
+        // If expanded, close the navigation menu
+        const togglerButton = document.querySelector(".navbar-toggler");
+        togglerButton.click(); // Programmatically click the toggler button
+      }
+    });
+  });
+});
