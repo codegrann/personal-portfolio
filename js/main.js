8cffe506149d6d51def2e13e133336d590bcda99
codegrann <omondihills02@gmail.com>
2023-09-04 14:28:08 +0300

    .

--- js/main.js
+++ js/main.js
@@ -6,3 +6,11 @@
 // document.write(location.href);
 // location.replace('www.github.com')
 document.write(history);
+
+document.querySelector("#navbar-toggler").onclick = function (e) {
+  e.target.innerHTML = <span class="navbar-toggler-icon"></span> ? (
+    <span class="navbar-toggler-icon">X</span>
+  ) : (
+    <span class="navbar-toggler-icon"></span>
+  );
+};
