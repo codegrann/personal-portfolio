146be67406ba4c1beb1fecd7c57d969f2b703027
codegrann <omondihills02@gmail.com>
2023-09-04 17:13:24 +0300

    .

--- js/main.js
+++ js/main.js
@@ -2,15 +2,17 @@ document.addEventListener("DOMContentLoaded", function () {
   const navLinks = document.querySelectorAll(".nav-link");
   const togglerButton = document.querySelector("#navbar-toggler");
   const navbarCollapse = document.getElementById("navbar-collapse-toggle");
-
+  console.log(window.innerWidth);
   // Attach a click event listener to each navigation link
-  navLinks.forEach(function (link) {
-    link.addEventListener("click", function () {
-      // Check if the navigation menu is expanded
-      if (navbarCollapse.style.display !== "none") {
-        // If expanded, close the navigation menu
-        togglerButton.click();
-      }
+  if (window.innerWidth < "768px") {
+    navLinks.forEach(function (link) {
+      link.addEventListener("click", function () {
+        // Check if the navigation menu is expanded
+        if (navbarCollapse.style.display !== "none") {
+          // If expanded, close the navigation menu
+          togglerButton.click();
+        }
+      });
     });
-  });
+  }
 });
