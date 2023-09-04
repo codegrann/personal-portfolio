9fad0edd2edaf9e70dd6cab778b6c1c48cd80963
codegrann <omondihills02@gmail.com>
2023-09-04 15:51:46 +0300

    .

--- js/main.js
+++ js/main.js
@@ -1,16 +1,1 @@
-// document.querySelector('body').style.backgroundColor="red";
-// window.open('https://app.surveoo.com/en/surveys', 'survey')
-// document.write(window.innerHeight)
-// document.write(document.documentElement.clientHeight)
-// document.write(location);
-// document.write(location.href);
-// location.replace('www.github.com')
-document.write(history);
 
-document.querySelector("#navbar-toggler").onclick = function (e) {
-  e.target.innerHTML = <span class="navbar-toggler-icon"></span> ? (
-    <span class="navbar-toggler-icon">X</span>
-  ) : (
-    <span class="navbar-toggler-icon"></span>
-  );
-};
